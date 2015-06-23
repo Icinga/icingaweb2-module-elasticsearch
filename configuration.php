3efd729df0348d23dfbddba43115103cc0187feb
Markus Frosch <markus@lazyfrosch.de>
2015-06-23 18:18:33 +0200

    Remove URL on menu Section

--- configuration.php
+++ configuration.php
@@ -1,10 +1,8 @@
 <?php
 
 $section = $this->menuSection('Logstash')
-    ->setUrl('logstash')
     ->setIcon('doc-text');
 
 $section->add(t('Event search'))
         ->setIcon('search')
         ->setUrl('logstash/event/search');
-
