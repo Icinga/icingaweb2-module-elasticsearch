a643edfd7c6bf1be459683c21cffeaeae19a6207
Johannes Meyer <johannes.meyer@netways.de>
2016-04-19 12:00:50 +0200

    Add base implementation for the REST API protocol
    refs #11616

---
+++ library/Elasticsearch/RestApi/RestApiClient.php
@@ -0,0 +1,208 @@
+<?php
+/* Icinga Web 2 Elasticsearch Module | (c) 2016 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Elasticsearch\RestApi;
+
+use ArrayIterator;
+use Icinga\Data\Extensible;
+use Icinga\Data\Filter\Filter;
+use Icinga\Data\Reducible;
+use Icinga\Data\Selectable;
+use Icinga\Data\Updatable;
+use Icinga\Exception\NotImplementedError;
+use Icinga\Exception\StatementException;
+
+class RestApiClient implements Extensible, Reducible, Selectable, Updatable
+{
+    /**
+     * The cURL handle of this RestApiClient
+     *
+     * @var resource
+     */
+    protected $curl;
+
+    /**
+     * The host of the API
+     *
+     * @var string
+     */
+    protected $host;
+
+    /**
+     * The name of the user to access the API with
+     *
+     * @var string
+     */
+    protected $user;
+
+    /**
+     * The password for the user the API is accessed with
+     *
+     * @var string
+     */
+    protected $pass;
+
+    /**
+     * The path of a file holding one or more certificates to verify the peer with
+     *
+     * @var string
+     */
+    protected $certificatePath;
+
+    /**
+     * Create a new RestApiClient
+     *
+     * @param   string  $host               The host of the API
+     * @param   string  $user               The name of the user to access the API with
+     * @param   string  $pass               The password for the user the API is accessed with
+     * @param   string  $certificatePath    The path of a file holding one or more certificates to verify the peer with
+     */
+    public function __construct($host, $user = null, $pass = null, $certificatePath = null)
+    {
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->certificatePath = $certificatePath;
+    }
+
+    /**
+     * Return the cURL handle of this RestApiClient
+     *
+     * @return  resource
+     */
+    public function getConnection()
+    {
+        if ($this->curl === null) {
+            $this->curl = $this->createConnection();
+        }
+
+        return $this->curl;
+    }
+
+    /**
+     * Create and return a new cURL handle for this RestApiClient
+     *
+     * @return  resource
+     */
+    protected function createConnection()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_FAILONERROR, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        if ($this->certificatePath !== null) {
+            curl_setopt($curl, CURLOPT_CAINFO, $this->certificatePath);
+        }
+
+        if ($this->user !== null && $this->pass !== null) {
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($curl, CURLOPT_USERPWD, $this->user . ':' . $this->pass);
+        }
+
+        return $curl;
+    }
+
+    /**
+     * Send the given request and return its response
+     *
+     * @param   RestApiRequest  $request
+     *
+     * @return  RestApiResponse
+     *
+     * @throws  RestApiException            In case an error occured while handling the request
+     */
+    public function request(RestApiRequest $request)
+    {
+        $scheme = strpos($this->host, '://') !== false ? '' : 'http://';
+        $path = '/' . ltrim($request->getPath(), '/');
+        $query = ($request->getParams()->isEmpty() ? '' : ('?' . (string) $request->getParams()));
+
+        $curl = $this->getConnection();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $request->getHeaders());
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $request->getMethod());
+        curl_setopt($curl, CURLOPT_URL, $scheme . $path . $query);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $request->getPayload());
+
+        $result = curl_exec($curl);
+        if ($result === false) {
+            throw new RestApiException(curl_errno($curl), curl_error($curl));
+        }
+
+        $response = new RestApiResponse(curl_getinfo($curl, CURLINFO_HTTP_CODE));
+        if ($result) {
+            $response->setPayload($result);
+            $response->setContentType(curl_getinfo($curl, CURLINFO_CONTENT_TYPE));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Create and return a new query for this RestApiClient
+     *
+     * @param   array   $indices    An array of index name patterns
+     * @param   array   $types      An array of document type names
+     *
+     * @return  RestApiQuery
+     */
+    public function select(array $indices = null, array $types = null)
+    {
+        throw new NotImplementedError('Queries are not supported yet');
+    }
+
+    /**
+     * Fetch and return all documents of the given query's result set using an iterator
+     *
+     * @param   RestApiQuery    $query  The query returning the result set
+     *
+     * @return  ArrayIterator
+     */
+    public function query(RestApiQuery $query)
+    {
+        throw new NotImplementedError('Queries are not supported yet');
+    }
+
+    /**
+     * Insert the given data for the given target
+     *
+     * @param   string|array    $target
+     * @param   array           $data
+     *
+     * @throws  StatementException
+     */
+    public function insert($target, array $data)
+    {
+        throw new NotImplementedError('Inserts are not supported yet');
+    }
+
+    /**
+     * Update the target with the given data and optionally limit the affected documents by using a filter
+     *
+     * Note that the given filter will have no effect in case the target represents a single document.
+     *
+     * @param   string|array    $target
+     * @param   array           $data
+     * @param   Filter          $filter
+     *
+     * @throws  StatementException
+     */
+    public function update($target, array $data, Filter $filter = null)
+    {
+        throw new NotImplementedError('Updates are not supported yet');
+    }
+
+    /**
+     * Delete entries in the given target, optionally limiting the affected entries by using a filter
+     *
+     * Note that the given filter will have no effect in case the target represents a single document.
+     *
+     * @param   string|array    $target
+     * @param   Filter          $filter
+     *
+     * @throws  StatementException
+     */
+    public function delete($target, Filter $filter = null)
+    {
+        throw new NotImplementedError('Deletions are not supported yet');
+    }
+}
---
+++ library/Elasticsearch/RestApi/RestApiRequest.php
@@ -0,0 +1,192 @@
+<?php
+/* Icinga Web 2 Elasticsearch Module | (c) 2016 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Elasticsearch\RestApi;
+
+use LogicException;
+use Icinga\Web\UrlParams;
+
+class RestApiRequest
+{
+    /**
+     * This request's headers
+     *
+     * @var array
+     */
+    protected $headers;
+
+    /**
+     * This request's method
+     *
+     * @var string
+     */
+    protected $method;
+
+    /**
+     * This request's path
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * This request's parameters
+     *
+     * @var UrlParams
+     */
+    protected $params;
+
+    /**
+     * This request's payload
+     *
+     * @var string
+     */
+    protected $payload;
+
+    /**
+     * The content type of this request's payload
+     *
+     * @var string
+     */
+    protected $contentType;
+
+    /**
+     * Set this request's headers
+     *
+     * @param   array   $headers
+     *
+     * @return  $this
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    /**
+     * Return this request's headers
+     *
+     * @return  array
+     */
+    public function getHeaders()
+    {
+        if ($this->headers === null) {
+            $this->headers = array();
+        }
+
+        if ($this->payload) {
+            $this->headers[] = sprintf('Content-Length: %u', strlen($this->payload));
+            if ($this->contentType) {
+                $this->headers[] = sprintf('Content-Type: ' . $this->contentType);
+            }
+        }
+
+        return $this->headers;
+    }
+
+    /**
+     * Set this request's method
+     *
+     * @param   string  $method
+     *
+     * @return  $this
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    /**
+     * Return this request's method
+     *
+     * @return  string
+     */
+    public function getMethod()
+    {
+        if ($this->method === null) {
+            throw new LogicException('It is required to explicitly set a method');
+        }
+
+        return $this->method;
+    }
+
+    /**
+     * Set this request's path
+     *
+     * @param   string  $path
+     *
+     * @return  $this
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * Return this request's path
+     *
+     * @return  string
+     */
+    public function getPath()
+    {
+        if ($this->path === null) {
+            $this->path = '';
+        }
+
+        return $this->path;
+    }
+
+    /**
+     * Set this requests's parameters
+     *
+     * @param   UrlParams   $params
+     *
+     * @return  $this
+     */
+    public function setParams(UrlParams $params)
+    {
+        $this->params = $params;
+        return $this;
+    }
+
+    /**
+     * Return this request's parameters
+     *
+     * @return  UrlParams
+     */
+    public function getParams()
+    {
+        if ($this->params === null) {
+            $this->params = new UrlParams();
+        }
+
+        return $this->params;
+    }
+
+    /**
+     * Set this request's payload
+     *
+     * @param   string  $data
+     * @param   string  $contentType
+     *
+     * @return  $this
+     */
+    public function setPayload($data, $contentType = null)
+    {
+        $this->payload = $data;
+        $this->contentType = $contentType;
+        return $this;
+    }
+
+    /**
+     * Return this request's payload
+     *
+     * @return  string
+     */
+    public function getPayload()
+    {
+        return $this->payload;
+    }
+}
