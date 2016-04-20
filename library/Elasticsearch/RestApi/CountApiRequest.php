b7b783719691796baedc82e5e4e0fd53ae5836bf
Johannes Meyer <johannes.meyer@netways.de>
2016-04-20 15:54:14 +0200

    Introduce class CountApiRequest
    refs #11616

---
+++ library/Elasticsearch/RestApi/CountApiRequest.php
@@ -0,0 +1,9 @@
+<?php
+/* Icinga Web 2 Elasticsearch Module | (c) 2016 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Elasticsearch\RestApi;
+
+class CountApiRequest extends SearchApiRequest
+{
+    const ENDPOINT = '_count';
+}
