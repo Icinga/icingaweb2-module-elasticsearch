d25df15aed9f2812a41f9174ed4dd2243a901319
Eric Lippmann <eric.lippmann@netways.de>
2017-09-07 11:06:12 +0200

    Add IniRepository for managing Elasticsearch instances

---
+++ library/Elasticsearch/Instances.php
@@ -0,0 +1,24 @@
+<?php
+/* Icinga Web 2 Elasticsearch Module (c) 2017 Icinga Development Team | GPLv2+ */
+
+namespace Icinga\Module\Elasticsearch;
+
+use Icinga\Repository\IniRepository;
+
+class Instances extends IniRepository
+{
+    protected $configs = [
+        'instances' => [
+            'name'      => 'instances',
+            'keyColumn' => 'name',
+            'module'    => 'elasticsearch'
+        ]
+    ];
+
+    protected $queryColumns = [
+        'instances' => [
+            'name',
+            'uri'
+        ]
+    ];
+}
