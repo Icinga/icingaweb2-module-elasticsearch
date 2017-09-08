e64411e723a6c5a9c99f69279b0b1ccb94b6bc1f
Eric Lippmann <eric.lippmann@netways.de>
2017-09-08 08:59:32 +0200

    Provide elasticsearch/config permission

--- configuration.php
+++ configuration.php
@@ -2,3 +2,8 @@
 /* Icinga Web 2 Elasticsearch Module (c) 2017 Icinga Development Team | GPLv2+ */
 
 /** @var Icinga\Application\Modules\Module $this */
+
+$this->providePermission(
+    'elasticsearch/config',
+    $this->translate('Allow to configure Elasticsaerch instances and event types')
+);
